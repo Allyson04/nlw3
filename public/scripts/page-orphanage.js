c3dee9ad6f0f179485ed4b2530b7681e592eb630
Allyson <allysoneduardo04@gmail.com>
2020-10-16 23:30:48 -0300

    feat: added orphanage.html option to active image be selected

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -24,3 +24,30 @@ const icon = L.icon({
 //criando e adicionando marcacao
 L.marker([-27.2058128,-49.6932569], { icon })
   .addTo(map)
+
+//   image Gallery
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    console.log(button.children)
+
+    //read all buttons
+    const buttons = document.querySelectorAll(".images button")
+    
+    //remove all active classes
+    buttons.forEach((button) => {
+        button.classList.remove(".active")
+        })
+
+    //select clicked images
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //refresh image container
+
+    imageContainer.src = image.src
+
+    //add active classes to clicked button
+    button.classList.add("active")
+}
